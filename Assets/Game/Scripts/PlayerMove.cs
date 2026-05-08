fadd3db81d1500dbd33026a03b29e7bb05fea56f
MoisesULima <jcumpa@ulima.edu.pe>
2026-05-08 16:12:33 -0500

    Update Project. Add Player Animations and basic movement

--- Assets/Game/Scripts/PlayerMove.cs
+++ Assets/Game/Scripts/PlayerMove.cs
@@ -2,20 +2,24 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    Rigidbody2D _body;
+
     void Awake()
     {
-        Debug.Log("Despertando al Player...");
+        // Detectamos y guardamos el RigidBody2D del player
+        _body = GetComponent<Rigidbody2D>();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Inicializar el player...");
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Actualiza el player, se llama muchas veces...");
+        // El personaje va a ir a una velocidad de 5 unidades de Unity por segundo
+        _body.linearVelocityX = 5f;
     }
 }
