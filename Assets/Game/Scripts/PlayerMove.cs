a80d1326f94261cfb90b56402e5d75e4d7a09d8c
MoisesULima <jcumpa@ulima.edu.pe>
2026-05-08 16:50:03 -0500

    Update Player Script, now it moves

--- Assets/Game/Scripts/PlayerMove.cs
+++ Assets/Game/Scripts/PlayerMove.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D _body;
+    // Acciones para el control
+    InputAction _moveAction;
 
     void Awake()
     {
         // Detectamos y guardamos el RigidBody2D del player
         _body = GetComponent<Rigidbody2D>();
+
+        _moveAction = InputSystem.actions["Player/Move"];
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,6 +25,11 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         // El personaje va a ir a una velocidad de 5 unidades de Unity por segundo
-        _body.linearVelocityX = 5f;
+        //_body.linearVelocityX = 5f;
+
+        // Guardamos el movimiento, de acuerdo a lo presionado en el teclado
+        Vector2 move = _moveAction.ReadValue<Vector2>();
+        // Mandamos los valores de x, a la velocidad x del player
+        _body.linearVelocityX = move.x * 5f;
     }
 }
